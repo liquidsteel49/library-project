1784f00296b8597481b58516f93a0db63018a3d2
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-26 11:06:34 -0500

    Added author table added the first of 4 tables that will be set. Author, Book, Borrower, and Books_borrowed. I had previously had books set up but had to undo that and make sure that I had properly set up my repos and all that jazz

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_194955) do
+ActiveRecord::Schema.define(version: 2018_11_26_160140) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.integer "copies"
-    t.string "lendable"
-    t.string "boolean"
+  create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
