7eb0f2a1abb14f1647beeae153f41e4b15f8b557
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-26 11:59:19 -0500

    added borrower scaffold Tried added books as well, but ran into problems. Going to leave this here for now and have lunch, then try again.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_160140) do
+ActiveRecord::Schema.define(version: 2018_11_26_161635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "authors", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "borrowers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
