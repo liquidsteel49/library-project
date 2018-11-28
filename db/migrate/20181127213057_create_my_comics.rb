231ad535e1758c50639ab197b557fe1773616f2a
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-28 15:22:58 -0500

    fixed migration of my_comics changed the "user_id" to user and same for comics

--- db/migrate/20181127213057_create_my_comics.rb
+++ db/migrate/20181127213057_create_my_comics.rb
@@ -1,8 +1,8 @@
 class CreateMyComics < ActiveRecord::Migration[5.2]
   def change
     create_table :my_comics do |t|
-      t.references :user_id, foreign_key: true
-      t.references :comic_id, foreign_key: true
+      t.references :user, foreign_key: true
+      t.references :comic, foreign_key: true
 
       t.timestamps
     end
