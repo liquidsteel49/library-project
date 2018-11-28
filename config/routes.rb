ff256e4857a23721265bdd4738974a5a70ac8442
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-28 15:53:19 -0500

    made exceptions to my_comics resources not allowing new or edit to be done with 'my_comics' table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :my_comics
+  resources :my_comics, except: %i[new edit]
   resources :comics, except: %i[new edit]
   # RESTful routes
   resources :examples, except: %i[new edit]
