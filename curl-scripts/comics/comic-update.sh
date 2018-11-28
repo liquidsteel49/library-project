aa90719a1d1e51792f03c37bb0e17b0823b8524b
liquidsteel49 <lowercasepeople49@gmail.com>
2018-11-28 15:19:13 -0500

    added all comic curl scripts

---
+++ curl-scripts/comics/comic-update.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+curl --include --request PATCH "http://localhost:4741/comics/${ID}" \
+--header "Content-Type: application/json" \
+--data '{
+  "comic": {
+    "title": "'"${TITLE}"'",
+    "writer": "'"${WRITER}"'",
+    "illustrator": "'"${ILLUSTRATOR}"'",
+    "is_series": "'"${SERIES}"'",
+    "issue_number": "'"${ISSUE}"'",
+    "series_total_issue": "'"${TOTAL}"'",
+    "ongoing": "'"${ONGOING}"'",
+    "genre": "'"${GENRE}"'",
+    "synopsis": "'"${SYNOPSIS}"'"
+  }
+}'
