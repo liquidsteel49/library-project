63835a50a4fb96a9e14c5a3dd3a23c34820ccfb1
liquidsteel49 <lowercasepeople49@gmail.com>
2018-12-04 09:21:15 -0500

    accounds for new comics table additions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_211436) do
+ActiveRecord::Schema.define(version: 2018_12_03_212945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2018_11_27_211436) do
     t.text "synopsis"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_comics_on_user_id"
   end
 
   create_table "examples", force: :cascade do |t|
@@ -47,5 +49,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_211436) do
     t.index ["token"], name: "index_users_on_token", unique: true
   end
 
+  add_foreign_key "comics", "users"
   add_foreign_key "examples", "users"
 end
